54ec4fc8e489ef5b9b0e65d1b80a74ca66202adc
dylanleclair <dylan.leclair@icloud.com>
2022-01-10 21:03:06 -0700

    Initial changes
    - Wrote out a script to execute on backend to generate a store of NASA APOD images to use in computation. Not using just the color scheme altho it would be more efficient so I can experiment with color matching algorithm it as the project develops.
    - Wrote out a general concept to help guide me as I continue to develop the app (see README.md)

---
+++ utils/download-apod.py
@@ -0,0 +1,31 @@
+# this script will download a huge set of images from the NASA APOD that we can use to make it work
+
+import requests
+import os
+import traceback 
+
+NUMBER_TO_DOWNLOAD=5
+
+list = requests.get("https://api.nasa.gov/planetary/apod?api_key=TEBAn743thZqnzWGLEm2daNnIvtMxrWpUInKAYr2&count=" + str(NUMBER_TO_DOWNLOAD) + "&thumbs=True")
+
+if not list.ok:
+    print("Failed to fetch APOD data.")
+    quit()
+
+list = list.json() # turn the data into JSON so we can work with it 
+
+# iterate through the returned JSON, find and download the images. 
+for x in list:
+    # check if the image exists in the output folder
+    # if it does not already, save it to the output folder
+    img = requests.get(x["url"])
+    filename = x["url"].split("/")[-1] # parse the filename so we can save the image according to it
+    ext = filename.split(".")[-1]
+    write_filename = x["date"] + "." + str(ext)
+    try:
+        f = open("APOD/" + write_filename, "wb")
+        f.write(img.content)
+        print("Wrote: " + write_filename)
+        f.close()
+    except Exception as e:
+        traceback.print_exc()
