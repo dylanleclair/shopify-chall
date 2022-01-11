17b951b533dcb7cf8897b4ef52068cf08b8b83d7
dylanleclair <dylan.leclair@icloud.com>
2022-01-10 21:17:05 -0700

    Created skeleton for script for later
    This script will populate the database / a file with the color schemes of the APOD images previously downloaded. See script comments for more detail.

---
+++ utils/generate-matrix.py
@@ -0,0 +1,11 @@
+# generate the color schemes of all of the images and save to a file.
+
+# this will help us with the next step of the project where we need to take the
+# users colour scheme and try to match it with every other color scheme, maximizing similarity.
+
+
+
+# open a directory, list the files in it. 
+
+# open each file, compute the color scheme and save it to a data file (or the data base) with the date of the image it corresponds to.
+
