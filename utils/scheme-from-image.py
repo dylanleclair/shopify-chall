ff1d3dbb5fe5962842a404a15d84cc8ac1fe72bc
dylanleclair <dylan.leclair@icloud.com>
2022-01-11 21:17:44 -0700

    Update scheme-from-image.py
    QOL update to use image specified in cmd line

--- utils/scheme-from-image.py
+++ utils/scheme-from-image.py
@@ -1,9 +1,9 @@
 
 import json 
 from colorthief import ColorThief
-
+import sys
 def get_scheme(path): # expects absolute path
     ct = ColorThief(path)
     return ct.get_palette(color_count=30)
 
-print(json.dumps(get_scheme("967724.jpg")))
+print(json.dumps(get_scheme(sys.argv[1])))
